b8aa68e7d0cdf942b438a6b968c9f1db19d86e0c
naif_alfardan <nayfsaad@gmail.com>
2022-05-13 16:46:20 +0300

    finial commit

---
+++ main.js
@@ -0,0 +1,21 @@
+let obj = document.getElementById("uMessage");
+let obj2 = document.getElementById("pMessage");
+let obj3=document.getElementById("password")
+function usrMessage() {
+    obj.style.display="inline";
+    obj2.style.display='none'
+}
+
+function pwdMessage() {
+    obj.style.display='none';
+    obj2.innerText='Write password';
+
+    obj2.style.display='inline';
+}
+function check(){
+    if (obj3.value.length===0){
+        obj2.innerText="enter a password can't submit without it"
+        obj2.style.display='inline';
+        event.preventDefault();
+    }
+}
--- sc.js
+++
@@ -1,3 +0,0 @@
-function usrm(){
-    Document.getelementb
-}
